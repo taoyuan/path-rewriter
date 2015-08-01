036d75cbee77fa4e2adcfc83af5a172dd586fea8
taoyuan <torworx@gmail.com>
2015-08-01 16:07:52 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# path-rewriter 
+[![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
+> A path rewriter for node.js
+
+## Install
+
+```sh
+$ npm install --save path-rewriter
+```
+
+## Examples
+
+Rewrite using a regular expression, rewriting `/i123` to `/items/123`.
+
+```js
+var rewriter = new Rewriter();
+rewriter.rule(/^\/i(\w+)/, '/items/$1');
+console.log(rewriter.rewrite('/i123'));  // '/items/123'
+```
+
+Rewrite using rule parameters, references may be named
+or numeric. For example rewrite `/foo..bar` to `/commits/foo/to/bar`:
+
+```js
+var rewriter = new Rewriter();
+rewriter.rule('/:src..:dst', '/commits/$1/to/$2');
+console.log(rewriter.rewrite('/foo..bar'));  // '/commits/foo/to/bar'
+
+var rewriter = new Rewriter();
+rewriter.rule('/:src..:dst', '/commits/:src/to/:dst');
+console.log(rewriter.rewrite('/foo..bar'));  // '/commits/foo/to/bar'
+```
+
+You may also use the wildcard `*` to soak up several segments,
+for example `/js/vendor/jquery.js` would become
+`/public/assets/js/vendor/jquery.js`:
+
+```js
+var rewriter = new Rewriter();
+rewriter.rule('/js/*', '/public/assets/js/$1');
+console.log(rewriter.rewrite('/js/vendor/jquery.js'));  // '/public/assets/js/vendor/jquery.js'
+```
+
+In the above examples, the original query string (if any) is left untouched.
+The regular expression is applied to the full url, so the query string
+can be modified as well:
+
+```js
+var rewriter = new Rewriter();
+rewriter.rule('/file\\?param=:param', '/file/:param');
+console.log(rewriter.rewrite('/file?param=foo'));  // '/file/foo'
+```
+
+The query string delimiter (?) must be escaped for the regular expression
+to work.
+
+## License
+
+MIT © [Tao Yuan]()
+
+[npm-image]: https://badge.fury.io/js/path-rewriter.svg
+[npm-url]: https://npmjs.org/package/path-rewriter
+[travis-image]: https://travis-ci.org/taoyuan/path-rewriter.svg?branch=master
+[travis-url]: https://travis-ci.org/taoyuan/path-rewriter
+[daviddm-image]: https://david-dm.org/taoyuan/path-rewriter.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/taoyuan/path-rewriter
+[coveralls-image]: https://coveralls.io/repos/taoyuan/path-rewriter/badge.svg
+[coveralls-url]: https://coveralls.io/r/taoyuan/path-rewriter
---
+++ test/rewriter.test.js
@@ -0,0 +1,118 @@
+'use strict';
+var t = require('chai').assert;
+var Rewriter = require('..');
+
+describe('rewriter', function () {
+  it('should rule rule with type', function () {
+    var rewriter = new Rewriter();
+    rewriter.rule('custom', '/foo', '/bar');
+    t.lengthOf(rewriter._rules, 1);
+    t.equal(rewriter._rules[0].type, 'custom');
+    t.equal(rewriter._rules[0].from, '/foo');
+    t.equal(rewriter._rules[0].to, '/bar');
+  });
+
+  it('should rule rule without type', function () {
+    var rewriter = new Rewriter();
+    rewriter.rule('/foo', '/bar');
+    t.lengthOf(rewriter._rules, 1);
+    t.equal(rewriter._rules[0].type, '*');
+    t.equal(rewriter._rules[0].from, '/foo');
+    t.equal(rewriter._rules[0].to, '/bar');
+  });
+
+  it('should rule rule using in', function () {
+    var rewriter = new Rewriter();
+    rewriter.in('/foo', '/bar');
+    t.lengthOf(rewriter._rules, 1);
+    t.equal(rewriter._rules[0].type, 'in');
+    t.equal(rewriter._rules[0].from, '/foo');
+    t.equal(rewriter._rules[0].to, '/bar');
+  });
+
+  it('should rule rule using out', function () {
+    var rewriter = new Rewriter();
+    rewriter.out('/foo', '/bar');
+    t.lengthOf(rewriter._rules, 1);
+    t.equal(rewriter._rules[0].type, 'out');
+    t.equal(rewriter._rules[0].from, '/foo');
+    t.equal(rewriter._rules[0].to, '/bar');
+  });
+
+  it('should throw assertion error if the source and the destination is not match', function () {
+    var rewriter = new Rewriter();
+    t.throw(function () {
+      rewriter.rule('/foo', '/bar/$1');
+    });
+  });
+
+  it('should rewrite using wild type', function () {
+    var rewriter = new Rewriter();
+    rewriter.rule('/foo', '/bar');
+    var p = rewriter.rewrite('/foo');
+    t.equal(p, '/bar');
+  });
+
+  it('should rewrite with custom type', function () {
+    var rewriter = new Rewriter();
+    rewriter.rule('custom', '/foo', '/bar');
+    var p = rewriter.rewrite('custom', '/foo');
+    t.equal(p, '/bar');
+  });
+
+  it('should rewrite match wild route', function () {
+    var rewriter = new Rewriter();
+    rewriter.rule('/foo', '/bar');
+    var p = rewriter.rewrite('custom', '/foo');
+    t.equal(p, '/bar');
+  });
+
+  it('should not rewrite without matching rule', function () {
+    var rewriter = new Rewriter();
+    rewriter.rule('custom1', '/foo', '/bar');
+    var p = rewriter.rewrite('custom2', '/foo');
+    t.equal(p, '/foo');
+  });
+
+  it('should rewrite correctly with named parameter', function () {
+    var rewriter = new Rewriter();
+    rewriter.rule('in', '/foo/:name', '/bar/:name');
+    var p = rewriter.rewrite('in', '/foo/hello');
+    t.equal(p, '/bar/hello');
+  });
+
+  it('should rewrite correctly with wildcard * parameter ', function () {
+    var rewriter = new Rewriter();
+    rewriter.rule('in', '/foo/*', '/bar/$1');
+    var p = rewriter.rewrite('in', '/foo/hello/world');
+    t.equal(p, '/bar/hello/world');
+  });
+
+  it('should rewrite correctly for destination without parameter ', function () {
+    var rewriter = new Rewriter();
+    rewriter.rule('in', '/foo/*', '/bar');
+    var p = rewriter.rewrite('in', '/foo/hello/world');
+    t.equal(p, '/bar');
+  });
+
+  it('should rewrite using a regular expression', function () {
+    var rewriter = new Rewriter();
+    rewriter.rule(/^\/i(\w+)/, '/items/$1');
+    var p = rewriter.rewrite('/i123');
+    t.equal(p, '/items/123');
+  });
+
+  it('should rewrite using route named parameters', function () {
+    var rewriter = new Rewriter();
+    rewriter.rule('/:src..:dst', '/commits/:src/to/:dst');
+    var p = rewriter.rewrite('/foo..bar');
+    t.equal(p, '/commits/foo/to/bar');
+  });
+
+  it('should rewrite using route numeric parameters', function () {
+    var rewriter = new Rewriter();
+    rewriter.rule('/:src..:dst', '/commits/$1/to/$2');
+    var p = rewriter.rewrite('/foo..bar');
+    t.equal(p, '/commits/foo/to/bar');
+  });
+});
